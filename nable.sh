24ea8e570e3e9b5cf87e1d29c3a5fd6f248bcee7
aporzio1 <aporzio1@me.com>
2022-02-23 15:32:43 -0500

    Create nable.sh

---
+++ nable.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://cdn.cloudbackup.management/maxdownloads/mxb-linux-x86_64.run
+chmod +x bm#df5c1784-7b8a-428f-b33c-5bc8013ab79c#137290#.run
+./bm#df5c1784-7b8a-428f-b33c-5bc8013ab79c#137290#.run 
