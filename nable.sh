660c4ea33703f03165d6e2e37ec7168e4ccd76a8
aporzio1 <aporzio1@me.com>
2022-02-23 17:24:22 -0500

    Update nable.sh

--- nable.sh
+++ nable.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt install wget
 wget https://cdn.cloudbackup.management/maxdownloads/mxb-linux-x86_64.run
 mv mxb-linux-x86_64.run bm#df5c1784-7b8a-428f-b33c-5bc8013ab79c#137290#.run 
 sleep 5
